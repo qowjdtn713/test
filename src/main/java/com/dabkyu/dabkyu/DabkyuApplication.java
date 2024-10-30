98dc0478d7a017e2c9d010a9b6903253a1bed47b
Enoch <gktjd425@gmail.com>
2024-10-30 10:10:34 +0900

    24.10.30_dabkyu_firstcommit

---
+++ src/main/java/com/dabkyu/dabkyu/DabkyuApplication.java
@@ -0,0 +1,13 @@
+package com.dabkyu.dabkyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DabkyuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DabkyuApplication.class, args);		
+	}
+
+}
